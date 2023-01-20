8a7db4e072c3b8654e93ada414d453940227c15d
seachelle18 <mhlchow@shaw.ca>
2023-01-20 16:29:02 -0700

    hello

--- game.php
+++ game.php
@@ -4,18 +4,23 @@ include "header.php";
 $gridHeight = 30;
 $gridWidth = 40;
 
-class gridbox {
+class Gridbox {
     protected $type;
+    public $img;
 }
 
 ?>
 
 <body>
     <div class="playable-area" color="red">
-        <?php for ($i = 0; $i < $gridWidth; $i++) {
-            echo '<div><button type="button"><img src = "tree.png"></button></div>';
-        }
-        echo '<br>';
+        <?php
+        for ($j = 0; $j < $gridHeight; $j++) { 
+            for ($i = 0; $i < $gridWidth; $i++) {
+                $${"grid" . $i . "_" . $j} = new Gridbox();
+                echo '<p>' . ${"grid" . $j . "_" . $j} . '</p>';
+                echo '<div><button type="button"><img src = "tree.png"></button></div>'; }
+            echo '<br>';
+            }
         ?>
     </div>
 </body>
