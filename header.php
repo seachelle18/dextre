3b05809c09450be454ec16c2a1cc42b4a6d47673
seachelle18 <mhlchow@shaw.ca>
2023-01-20 15:31:25 -0700

    add header

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>BC Nature Game</title>
+    <link rel="stylesheet" type="text/css" href="inc/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="bc, nature, game, quiz, learning">
+    <meta name="description" content="BC Nature Game">
+    <meta name="author" content="Dextre">
+</head>
+<header>
+    <div class="main-header">
+        <h1>
+            BC Learning Game
+        </h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+        </ul>
+    </div>
+</header>
