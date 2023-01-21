637ff7cc6ff0c80484f79bcf7ea92ded46a700c1
seachelle18 <mhlchow@shaw.ca>
2023-01-20 18:22:13 -0700

    alsdkfjsdkfj

--- quiz.php
+++ quiz.php
@@ -1,6 +1,10 @@
 <?php
 include "header.php";
-$questionArray = ["question 1", "question 2", "question 3", "question 4", "question 5", "question 6", "question 7", "question 8", "question 9", "question 10"];
+//$questionArray = ["question 1", "question 2", "question 3", "question 4", "question 5", "question 6", "question 7", "question 8", "question 9", "question 10"];
+
+
+$SELECT_QUESTIONS = "SELECT * FROM bank LIMIT 10";
+$questionArray = $conn->query($SELECT_QUESTIONS);
 
 ?>
 
@@ -8,10 +12,13 @@ $questionArray = ["question 1", "question 2", "question 3", "question 4", "quest
     <div class="container">
         <form method="POST">
             <?php foreach ($questionArray as $number => $question) {
-                echo '<label class="question-label" for="' . $number . '" ' . 'class="question-label">' . $question . '</label>';
-                echo '<input class = "question-response" type="text" name="' . $number . '">'; }
-            ?>
-            <input type="submit" name="submit-button" value="Submit">            
+                echo '<div class="question-box">';
+                echo '<div class="label-container"><label class="question-label" for="' . $number . '" ' . 'class="question-label">' . $question . '</label></div>';
+                echo '<div class="question-container"><input class="question-response" type="text" name="' . $number . '"></div>'; 
+                echo '</div>'; }
+                echo '<input class="submit-button" type="submit" name="submit-button" value="Submit">';
+                echo '</div>';
+            ?>            
         </form>
     </div>
 </body>
