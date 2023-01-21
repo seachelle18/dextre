ca5d4570be7ef151d94c1b2af4055f098b034ce9
seachelle18 <mhlchow@shaw.ca>
2023-01-20 17:45:00 -0700

    hellooooo

--- quiz.php
+++ quiz.php
@@ -5,11 +5,13 @@ $questionArray = ["question 1", "question 2", "question 3", "question 4", "quest
 ?>
 
 <body>
-    <form method="POST">
-        <?php foreach ($questionArray as $number => $question) {
-            echo '<label class="question-label" for="' . $number . '" ' . 'class="question-label">' . $question . '</label>';
-            echo '<input class = "question-response" type="text" name="' . $number . '">'; }
-        ?>
-        <input type="submit" name="submit-button" value="Submit">            
-    </form>
+    <div class="container">
+        <form method="POST">
+            <?php foreach ($questionArray as $number => $question) {
+                echo '<label class="question-label" for="' . $number . '" ' . 'class="question-label">' . $question . '</label>';
+                echo '<input class = "question-response" type="text" name="' . $number . '">'; }
+            ?>
+            <input type="submit" name="submit-button" value="Submit">            
+        </form>
+    </div>
 </body>
